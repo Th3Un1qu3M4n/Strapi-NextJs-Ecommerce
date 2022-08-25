da553da2540bda3fbc753d5c3b40f602e54b36e3
Muhammad Ahmed <ahmed495802@gmail.com>
2022-08-25 20:09:12 +0500

    uploaded: Strapi with NextJs Ecommerce

---
+++ config/database.js
@@ -0,0 +1,27 @@
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi-store'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'admin'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required for postgres
+    },
+    debug: false,
+  },
+});
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
